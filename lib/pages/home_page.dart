175f0d4cb1105027bf45dbceb3296c2e2734b5eb
shaheen17cdm <shaheenmhd2024@gmail.com>
2024-12-13 04:39:43 +0530

    second commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,66 @@
 import 'package:chat_app/components/my_drawer.dart';
+import 'package:chat_app/components/user_tile.dart';
+import 'package:chat_app/pages/chat_page.dart';
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+   HomePage({super.key});
 
-  
+  //chat and auth service
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(title: Padding(
-        padding: const EdgeInsets.only(right: 40),
+      appBar: AppBar(title: const Padding(
+        padding: EdgeInsets.only(right: 40),
         child: Center(child: Text('Home')),
       ),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+
+  //build a list of users except for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUsersStream(),
+       builder: (context, snapshot) {
+        //errors
+        if (snapshot.hasError) {
+          return const Text('Error');
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading...');
+        }
+
+        //return list view
+        return ListView(
+          children: snapshot.data!.map<Widget>((userData) => _buildUserListItem(userData, context)).toList(),
+        );
+       });
+  }
+
+  //build individual list tile for user
+  Widget _buildUserListItem(Map<String, dynamic> userData, BuildContext context) {
+    //display all users except current user
+   if (userData['email'] != _authService.getCurrentUser()!.email) {
+     return UserTile(text: userData['email'],
+    onTap: (){
+      Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage(recieverEmail: userData['email'],),
+      ));
+    },
+    );
+   }
+   else {
+    return Container();
+   }
   }
 }
